99faa2f133cf18fa7a3194e038e5f5d5e44c9d37
silvieruzzie <169399791+silvieruzzie@users.noreply.github.com>
2024-10-10 21:25:19 +0200

    Worked on Home screen and custom hook for fetching.

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,19 +1,36 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlatList, Image, Text, View } from "react-native";
+import { Alert, FlatList, Image, Text, View, RefreshControl } from "react-native";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import { images } from "../../constants";
-import EmptyState from "../../components";
-
+import EmptyState from "../../components/EmptyState";
+import VideoCard from "../../components/VideoCard";
+import { useState } from "react";
+import useAppwrite from "../../lib/useAppwrite";
+import { getAllPosts } from "../../lib/appwrite";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView className="bg-primary">
-      <FlatList 
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]} 
-        keyExtractor={(item) => item.id.toString()} 
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            username={item.username}
+          />
         )}
         ListHeaderComponent={() => (
           <View className="flex my-6 px-4 space-y-6">
@@ -23,10 +40,9 @@ const Home = () => {
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
+                  Silvia
                 </Text>
               </View>
-
               <View className="mt-1.5">
                 <Image
                   source={images.logoSmall}
@@ -37,24 +53,26 @@ const Home = () => {
             </View>
 
             <SearchInput />
-          <View className="w-full flex-1 pt-5 pb-8">
-            <Text className="text-white text-grade-100 text-lg font-pregular mb-3">
-              Latest Videos
-            </Text>
-
-            <Trending posts={[{id: 1}, {id: 2}, {id: 3}] ?? []} />
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
+              <Trending posts={posts ?? []} />
+            </View>
           </View>
-        </View>
         )}
         ListEmptyComponent={() => (
-          <EmptyState 
+          <EmptyState
             title="No Videos Found"
             subtitle="Be the first one to upload a video"
           />
         )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
 };
 
-export default Home
+export default Home;
